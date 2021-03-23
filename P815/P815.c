e51c70d80761c2319a4075854b802aa63d5c65ef
Coin_stars <192354134@qq.com>
2021-03-24 01:34:58 +0800

    2321/3/24

--- P815/P815.c
+++ P815/P815.c
@@ -5,7 +5,7 @@
 #define MAX_LINE_LEN 81
 
 /* userCode(<50字符): 自定义函数之原型声明 */
-void sortP_Str(pstr);
+void sortP_Str(char* pstr[MAX_LINE]);
 
 int main(void)
 {
@@ -34,7 +34,20 @@ int main(void)
 }
 
 /* User Code Begin(考生在此后完成自定义函数的设计，行数不限) */
-void sortP_Str(pstr)
+void sortP_Str(char* pstr[MAX_LINE])
 {
-
+	char temp[MAX_LINE_LEN];
+	int i, j;
+	for (i = 0; i < MAX_LINE - 1; i++)
+	{
+		for (j = 0; j < MAX_LINE - 1 - i; j++)
+		{
+			if (strcmp(pstr[j], pstr[j + 1]) > 0)
+			{
+				strcpy(temp, pstr[j]);
+				strcpy(pstr[j], pstr[j + 1]);
+				strcpy(pstr[j + 1], temp);
+			}
+		}
+	}
 }
--- TEST/TEST.c
+++ TEST/TEST.c
@@ -1,9 +1,49 @@
-#include <stdio.h>
-#include<string.h>
-#include<windows.h>
+#include<stdio.h>
+
+void StringBubbleSort(char* MyStrings[], int iCount, int iSortType)
+{
+    int i, j;
+    char* temp;
+    for (i = 0; i < iCount - 1; i++)
+    {
+        for (j = i + 1; j < iCount; j++)
+        {
+            if (iSortType == 1)
+            {
+                if (strcmp(MyStrings[j], MyStrings[i]) < 0)
+                {
+                    temp = MyStrings[i];
+                    MyStrings[i] = MyStrings[j];
+                    MyStrings[j] = temp;
+
+                }
+            }
+            else
+            {
+                if (strcmp(MyStrings[j], MyStrings[i]) > 0)
+                {
+                    temp = MyStrings[i];
+                    MyStrings[i] = MyStrings[j];
+                    MyStrings[j] = temp;
+
+                }
+            }
+        }
+    }
+}
+
 int main()
 {
-	char ch[3][4] = { "abc","CU","X" };
+    int i, iSortType;
+    int argc = 3;
+    char* argv[] = { "dhisawkfgka","dwehlifdwie","fwehiulyfwe" };
 
-	return 0;
+    iSortType = strlen(*argv); //第一个参数 排序方式 
+
+    //StringBubbleSort(argv,strlen(*argv),iSortType); //排序处理 
+
+    for (i = 0; i < argc; i++) //显示排序结果 
+        printf("%s\n", *(argv+i));
+    return 1;
+
 }
